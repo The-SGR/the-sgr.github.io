05a0b84a2e4916800d91d94817a9017e6f105d0c
SGR <113258080+The-SGR@users.noreply.github.com>
2025-05-30 23:38:00 +0300

    §

--- js/dropdown.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector('#dropbtn');
-const menu = document.querySelector('.dropdown-content')
-
-if (menu) {
-    btn.addEventListener('click', () => {
-        menu.classList.toggle('disp');
-    })
-}
